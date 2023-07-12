e0f8cfa444655b00e01183e010463724d6f80bbf
Martina Basile <mbasile@e4r3p4.42roma.it>
2023-07-12 17:47:38 +0200

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbasile <mbasile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 12:28:01 by mbasile           #+#    #+#             */
-/*   Updated: 2023/07/12 17:05:15 by mbasile          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:46:08 by mbasile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,38 +28,56 @@ int	ft_checkinput(char *str)
 	return (1);
 }
 
-int	main(int ac, char **av)
+void	ft_freematrix(char **matrix)
 {
-	char	**matrix;
-	t_stack	*stack;
 	int	i;
 
-	matrix = NULL;
+	i = -1;
+	while (matrix[++i])
+		free(matrix[i]);
+	free(matrix);
+}
+
+void	init_one(t_stack *stack, char **av)
+{
+	char	**matrix;
+	int	i;
+
+	matrix = ft_split(av[1], ' ');
+	i = -1;
+	while (matrix[++i])
+	{
+		if (!ft_checkinput(matrix[i]))
+		{
+			ft_freematrix(matrix);
+			ft_error();
+		}
+	}
+	stack->size_a = i;
+	stack->size_b = 0;
+	stack->a = ft_calloc(i, sizeof(int));
+	stack->b = ft_calloc(i, sizeof(int));
+	i = -1;
+	while (matrix[++i])
+	{
+		if (ft_atoi(matrix[i]) > 2147483647 || ft_atoi(matrix[i]) < -2147483648)
+		{
+			ft_freematrix(matrix);
+			ft_error();
+		}
+		stack->a[i] = ft_atoi(matrix[i]);
+	}
+	ft_freematrix(matrix);
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*stack;
+
 	stack = ft_calloc(1, sizeof(t_stack));
 	if (ac == 2)
 	{
-		i = 0;
-		matrix = ft_split(av[1], ' ');
-		while (matrix[i])
-		{
-			if (!ft_checkinput(matrix[i]))
-				ft_error();
-			i++;
-		}
-		stack->size_a = i;
-		stack->size_b = 0;
-		stack->a = ft_calloc(i, sizeof(int));
-		stack->b = ft_calloc(i, sizeof(int));
-		i = 0;
-		while (matrix[i])
-		{
-			if (ft_atoi(matrix[i]) > 2147483647 || ft_atoi(matrix[i]) < -2147483648)
-				ft_error();
-			stack->a[i] = ft_atoi(matrix[i]);
-			printf("%d\n", stack->a[i]);
-			i++;
-		}
-		
+		init_one(stack, av);
 		printf("%d\n", i);
 		// COTROLLO IS_DIGIT CON CARATTERE - DAVANTI (AV[X][0])
 		// conteggio dello stack di A
